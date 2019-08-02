94b6f1b67c84b4c0656f36183e297bff7162e006
enisberk <EnisBerk@users.noreply.github.com>
2019-08-01 23:51:08 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-# Copyright (C) 2018 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Setup file for lottery ticket hypothesis."""
-
-from setuptools import setup
+from setuptools import setup,find_packages
 
 SHORT_DESCRIPTION = """
 An implementation of the lottery ticket hypothesis experiment.""".strip()
@@ -29,13 +13,11 @@ DEPENDENCIES = [
 ]
 
 VERSION = '1'
-URL = 'https://github.com/brain-research/lottery-ticket'
 
 setup(
-    name='lottery-ticket',
+    name='lottery_ticket',
     version=VERSION,
     description=SHORT_DESCRIPTION,
-    url=URL,
 
     author='Jonathan Frankle',
     author_email='jfrankle@google.com',
@@ -64,8 +46,11 @@ setup(
     ],
 
     keywords='lottery ticket hypothesis',
-
-    packages=['lottery-ticket'],
+    packages=['lottery_ticket', 'lottery_ticket.foundations', 'lottery_ticket.datasets', 'lottery_ticket.mnist_fc'],
+    package_dir={'lottery_ticket': '.',
+                 'lottery_ticket.foundations': './foundations',
+                 'lottery_ticket.datasets': './datasets',
+                 'lottery_ticket.mnist_fc': './mnist_fc'},
 
     install_requires=DEPENDENCIES,
 )
